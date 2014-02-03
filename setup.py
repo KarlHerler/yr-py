a19879af95b431e4b41eb8eb46d21d9da12ff3a8
Karl Herler <hi@karlherler.com>
2014-02-03 10:45:50 +0200

    Added a simple setup.py file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='yr',
+    version='0.1',
+    packages=['yr',],
+    license='MIT',
+    description='An unofficial python client for the yr.no weather service',
+    url='https://github.com/KarlHerler/yr-py',
+    author='Karl Herler',
+    author_email='hi@karlherler.com',
+    long_description=open('README').read(),
+)
